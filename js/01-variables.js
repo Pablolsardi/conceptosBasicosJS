c3a221fa87e9d5a708bcb10e480a3781c5054a38
Pablo Lopez Sardi <lopezsardip@gmail.com>
2023-10-23 18:36:26 -0300

    agregamos unas variables para practicar, y concatenamos

---
+++ js/01-variables.js
@@ -0,0 +1,17 @@
+// Esto es un comentario de única línea
+
+/*Este es un comentario 
+de múliples
+líneas*/ 
+
+// Declarar una variable
+
+let currentYear=2023;
+
+const pi=3.141592
+
+// Mostrar mensajes
+
+console.log(currentYear);
+console.log("hola putos");
+console.log('estamos en ' + currentYear);
