5cef16784f48a9650ca996aecc88188e2e8cea35
Pablo Lopez Sardi <lopezsardip@gmail.com>
2023-10-25 17:21:31 -0300

    agregamos alert, y document.write

--- js/01-variables.js
+++ js/01-variables.js
@@ -7,11 +7,18 @@ líneas*/
 // Declarar una variable
 
 let currentYear=2023;
+const universe = 'https://universe.rollingcodeschool.com/';
 
-const pi=3.141592
+const pi=3.141592;
 
 // Mostrar mensajes
 
 console.log(currentYear);
 console.log("hola putos");
 console.log('estamos en ' + currentYear);
+console.log(universe);
+
+document.write('Esto es una prueba de document.write');
+document.write('<p class="parrafoDestacado">Esto es una prueba de document.write</p>');
+
+alert("hola mundo");
