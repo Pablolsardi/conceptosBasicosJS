54b8a0b52b3293457b09c3d05d6d05f028d430f6
Pablo Lopez Sardi <lopezsardip@gmail.com>
2023-11-13 15:55:19 -0300

    Agregamos métodos para eliminar elementos del array

--- js/05-arrays.js
+++ js/05-arrays.js
@@ -1,6 +1,6 @@
 // Declarar un array
 const personajes = [];
-const peliculas = ['Interstellar','Matrix','lotr',2023,true,'los juegos del hambre']   
+const peliculas = ['Interstellar','Matrix','lotr',2023,true,'Los juegos del hambre']   
 
 // Mostrar un array
 console.log(peliculas);
@@ -42,6 +42,46 @@ document.write(`</ul>`);
 
 peliculas.push('Gran Turismo');
 document.write(`<h2>Agregamos GT (${peliculas.length})</h2>`);
+document.write(`<ul>`);
+for (let iPeliculas = 0 ; iPeliculas<peliculas.length ; iPeliculas++){
+    document.write(`<li>${peliculas[iPeliculas]}</li>`);
+}
+document.write(`</ul>`);
+
+// Modificar un elementos
+
+peliculas[6] = 'Inception';
+document.write(`<h2>Modificamos el elemento [6] (${peliculas.length})</h2>`);
+document.write(`<ul>`);
+for (let iPeliculas = 0 ; iPeliculas<peliculas.length ; iPeliculas++){
+    document.write(`<li>${peliculas[iPeliculas]}</li>`);
+}
+document.write(`</ul>`);
+
+// Eliminar elementos del array
+
+peliculas.shift();
+document.write(`<h2>Eliminamos primer elemento del array (${peliculas.length})</h2>`);
+document.write(`<ul>`);
+for (let iPeliculas = 0 ; iPeliculas<peliculas.length ; iPeliculas++){
+    document.write(`<li>${peliculas[iPeliculas]}</li>`);
+}
+document.write(`</ul>`);
+
+// Eliminar elementos del medio del array
+
+peliculas.splice(6,1);
+document.write(`<h2>Eliminamos True del array (${peliculas.length})</h2>`);
+document.write(`<ul>`);
+for (let iPeliculas = 0 ; iPeliculas<peliculas.length ; iPeliculas++){
+    document.write(`<li>${peliculas[iPeliculas]}</li>`);
+}
+document.write(`</ul>`);
+
+// Eliminar el último elemento del array
+
+peliculas.pop();
+document.write(`<h2>Eliminamos el último elemento del array(${peliculas.length})</h2>`);
 document.write(`<ul>`);
 for (let iPeliculas = 0 ; iPeliculas<peliculas.length ; iPeliculas++){
     document.write(`<li>${peliculas[iPeliculas]}</li>`);
